b0f15b9427f09d5b5fc071439bfae30ed064083c
James Gung <James.Gung@ipsoft.com>
2016-01-13 15:52:49 -0500

    # replaces colon for original locations

--- src/main/java/edu/emory/clir/clearnlp/lexicon/propbank/PBLocation.java
+++ src/main/java/edu/emory/clir/clearnlp/lexicon/propbank/PBLocation.java
@@ -1,26 +1,18 @@
 /**
- * Copyright 2014, Emory University
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2014, Emory University <p/> Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at <p/> http://www.apache.org/licenses/LICENSE-2.0
+ * <p/> Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 package edu.emory.clir.clearnlp.lexicon.propbank;
+
+import java.io.Serializable;
+import java.util.regex.Pattern;
 
 import edu.emory.clir.clearnlp.util.StringUtils;
 import edu.emory.clir.clearnlp.util.constant.PatternConst;
 import edu.emory.clir.clearnlp.util.constant.StringConst;
-
-import java.io.Serializable;
-import java.util.regex.Pattern;
 
 /**
  * @author Jinho D. Choi ({@code jinho.choi@emory.edu})
@@ -126,11 +118,13 @@ public class PBLocation implements Serializable, Comparable<PBLocation> {
     public String toString() {
         StringBuilder build = new StringBuilder();
 
-        if (original)
-            build.append(ORIGINAL_LOC);
+
         build.append(s_type);
         build.append(i_terminalID);
-        build.append(DELIM);
+        if (original)
+            build.append(ORIGINAL_LOC);
+        else
+            build.append(DELIM);
         build.append(i_height);
 
         return build.toString();
